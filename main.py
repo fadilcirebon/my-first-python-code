6b4fbfc159bef5c2bd0ab15a2f3fa3980eec61ab
fadil <fadil.rich@gmail.com>
2021-11-18 14:04:57 +0700

    with ssh

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 """ini adalah project pertama dengan github"""
 print ("hello world")
 print ("my name fadil")
+
+for i in range (1,200):
+    if(i%2==0):
+        print("Angka genap",i)
+    else:
+         print("Angka ganjil",i)
