d037c70a1c6eefec4a216cb32774c15dc40a7291
fadil <fadil.rich@gmail.com>
2021-12-02 10:23:20 +0700

    fundamental

---
+++ Tipe data list.py
@@ -0,0 +1,18 @@
+daftar_buku = ('Seven Habits', 'How to Influence People', 'First Things First', 'wiro sableng', 'kamasutra','teknik perkewean')
+print('Tampilkan variable daftar_buku')
+print(daftar_buku)
+
+print('nProses semua dengan for in')
+for buku in daftar_buku:
+    print(buku)
+
+print('/nTampilkan isi daftar_buku pada indeks tertentu')
+print(daftar_buku[0])
+print(daftar_buku[1])
+print(len(daftar_buku))
+
+
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+
